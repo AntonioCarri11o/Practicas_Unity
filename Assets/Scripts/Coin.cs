9c1e4ff0ab63339ac535d0414d16c784b8a0b5c7
MiguelSP040 <20213tn040@utez.edu.mx>
2024-10-03 15:32:24 -0600

    Audio

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public AudioSource coin;
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log(" +1 ");
+            coin.PlayOneShot(coin.clip);
             Destroy(this.gameObject);
         }
     }
